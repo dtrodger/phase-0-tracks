c47e492e2c1eab8ed68780dce0729523c5bf7548
David Rodgers <dtrodger20@gmail.com>
2016-08-02 14:24:47 -0500

    added comments

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,4 @@
-
+# defines a method that check if the input can be converted into an integer
 def is_integer input
 	true if Integer(input) rescue false
 end
@@ -8,37 +8,45 @@ puts "\nWelcome to the Werewolf Intelligence Bureau database"
 print "\nEnter the number of employees to be processed: "
 num_employees = gets.chomp
 
+# asks the user to enter the number of employees to verify until they enter an integer
 until is_integer(num_employees)
 	print "Invalid entry. Enter an integer: "
 	num_employees = gets.chomp
 end
 
+# starts a for loop that will run as many times as the user specified in num_employees
 for i in (1..num_employees.to_i)
 
 	puts "\nEmployee Number #{i}\n"
 
+	# get employee name
 	print "Enter employee name: "
 	name = gets.chomp
 
 	allergies = Array.new
 
+	# get employee allergy
 	print "Enter employee allegy or 'done' when all allergies have been entered: "
 	new_allergy = gets.chomp
 
+	# continue asking for allergies and saving them in an array until the user enters 'done' or 'sunshine'
 	until new_allergy == 'sunshine' or new_allergy == 'done'
 		allergies.push(new_allergy)
 		print "Enter the next allergy or 'done': "
 		new_allergy = gets.chomp
 	end
 
+	# if the employee is allergic to sunshine, print a message and move on to the next iteration in the for loop
 	if new_allergy == 'sunshine'
 		puts "#{name} is probably a vampire"
 		next
 	end
 
+	# get the employees age
 	print "Enter employee age: "
 	age = gets.chomp
 	
+	# asks the user to enter their age to until they enter an integer
 	while not is_integer(age)
 		print "Invalid entry. Enter an Integer: "
 		age = gets.chomp
@@ -46,9 +54,11 @@ for i in (1..num_employees.to_i)
 
 	age = age.to_i
 
+	# get the employees birth year
 	print "Enter employee birth year: "
 	birth_year = gets.chomp
 
+	# asks the user to enter their birth year to until they enter an integer
 	while not is_integer(birth_year)
 		print "Invalid entry. Enter an Integer: "
 		birth_year = gets
@@ -58,15 +68,18 @@ for i in (1..num_employees.to_i)
 
 	current_year = Time.now.year
 	
+	# check if the employees age is the same as the current year minus the year the employee said they were born
 	if (current_year - birth_year) != age
 		age_correct = false
 	else
 		age_correct = true
 	end
 
+	# check ask the employee if they like garlic bread
 	print "Would the employee like garlic bread with their weekly free pasta lunch? (Enter yes or no): "
 	likes_garlic = gets.chomp.downcase
 
+	# if the user didn't enter yes or no about the garlic bread, continue to ask them
 	until likes_garlic == "yes" || likes_garlic == "no"
 		print "Invalid entry. Enter yes or no: "
 		likes_garlic = gets.chomp.downcase
@@ -78,9 +91,11 @@ for i in (1..num_employees.to_i)
 		likes_garlic = true
 	end
 
+	# check ask the employee if they want insurance
 	print "Would the employee like to sign up for the company insurance? (Enter yes or no): "
 	wants_insurance = gets.chomp.downcase
 
+	# if the user didn't enter yes or no about insurance, continue to ask them
 	until wants_insurance == "yes" || wants_insurance == "no"
 		print "Invalid entry. Enter yes or no: "
 		wants_insurance = gets.chomp.downcase
@@ -92,6 +107,7 @@ for i in (1..num_employees.to_i)
 		wants_insurance = true
 	end
 
+	# print a message specifying whether or not the employee is a vampire
 	if name == "Drake Cula" || name == "Tu Fang"
 		puts "#{name} is definitely a vampire"
 	elsif !wants_insurance && !likes_garlic && !age_correct
