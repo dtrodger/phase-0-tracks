6aa3a6ed0d3edbb2202cb66e2e90f2e016939a76
David Rodgers <dtrodger20@gmail.com>
2016-08-10 14:07:12 -0500

    added pseudocode for interior designer client info form

---
+++ ruby/hashes.rb
@@ -0,0 +1,14 @@
+
+# method with a key a value and a hash as inputs and the key and value to the hash then returns the hash
+
+def key_vale_to_hash (key, value, hash)
+	hash[key] = value
+	hash
+end
+
+# method that takes a string input and returns true if it is an integer and false if it is not
+
+# method that takes  string as an input and returns true if the input is yes, false if the input is no and nil if the input is neither yes or no
+
+
+#UI
