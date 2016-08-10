1db3c70044ae902a3be1151241d809b575d24736
David Rodgers <dtrodger20@gmail.com>
2016-08-10 14:38:45 -0500

    adds prompt for user input and input validation

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -9,8 +9,6 @@ end
 # method that takes a string input and returns true if it is an integer and false if it is not
 
 def true_false (string)
-	string = string.downcase
-
 	if string == "yes"
 		true
 	elsif string == "no"
@@ -28,6 +26,67 @@ end
 
 #UI
 
-puts true_false("Yes")
-puts true_false("No")
-puts true_false("maybe")
+#the clients name, age, number of children, decor theme, cats or dogs, favorite color, vintage or modern"
+
+client_hash = Hash.new
+
+#ask user for name
+
+print "Name: "
+name = gets.chomp
+
+client_hash = key_vale_to_hash(:name, name, client_hash)
+
+print "Age: "
+age = gets.chomp
+
+until is_integer(age)
+	print "(Error Enter an Integer) Age: "
+	age = gets.chomp
+	age
+end
+
+client_hash = key_vale_to_hash(:age, age, client_hash)
+
+# ask user for number of children
+
+print "Number of Children: "
+num_child = gets.chomp
+
+until is_integer(num_child)
+	print "(Error Enter an Integer) Number of Children: "
+	num_child = gets.chomp
+	num_child
+end
+
+# ask user for a decor theme
+
+client_hash = key_vale_to_hash(:num_child, num_child, client_hash)
+
+print "Decor Theme: "
+theme = gets.chomp
+
+client_hash = key_vale_to_hash(:theme, theme, client_hash)
+
+# ask user if they have cats or dogs
+
+print "Cats or Dogs (Yes or No): "
+cats_or_dogs = gets.chomp
+cats_or_dogs = cats_or_dogs.downcase
+
+while true_false(cats_or_dogs) == nil
+	print "(Error Enter Yes or No) Cats or Dogs: "
+	cats_or_dogs = gets.chomp
+	cats_or_dogs = cats_or_dogs.downcase
+	cats_or_dogs
+end
+
+cats_or_dogs = true_false(cats_or_dogs)
+
+client_hash = key_vale_to_hash(:cats_or_dogs, cats_or_dogs, client_hash)
+
+
+
+
+
+
