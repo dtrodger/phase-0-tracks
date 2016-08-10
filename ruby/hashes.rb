a16078ff222dc7431525f9d30baf20d646f85b92
David Rodgers <dtrodger20@gmail.com>
2016-08-10 14:13:58 -0500

    adds method that checks if string is yes/no returns true/false/nil

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -8,6 +8,18 @@ end
 
 # method that takes a string input and returns true if it is an integer and false if it is not
 
+def true_false (string)
+	string = string.downcase
+
+	if string == "yes"
+		true
+	elsif string == "no"
+		false
+	else
+		nil
+	end
+end
+
 # method that takes  string as an input and returns true if the input is yes, false if the input is no and nil if the input is neither yes or no
 
 def is_integer input
@@ -16,5 +28,6 @@ end
 
 #UI
 
-puts is_integer("d")
-puts is_integer("2")
+puts true_false("Yes")
+puts true_false("No")
+puts true_false("maybe")
