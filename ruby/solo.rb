cf33f3f371bd50cf024b03f5f79f89f28358e396
David Rodgers <dtrodger20@gmail.com>
2016-08-16 13:06:38 -0500

    pseudocode car class

---
+++ ruby/solo.rb
@@ -0,0 +1,9 @@
+# CAR MAKER
+
+# Initialized attributes (make, model, miles, type, color, year, new?)
+
+# Method to make the car drive and add miles
+
+# Method to output attributes in one call
+
+# Method to rev the engine
