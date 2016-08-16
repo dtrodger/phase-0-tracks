a027a8761efab22e557bfa90480a0a27b5a7533b
David Rodgers <dtrodger20@gmail.com>
2016-08-16 11:29:33 -0500

    adds attributes and about method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,21 @@
 class Santa
-
-	def initialize ()
+	def initialize (gender, ethnicity)
 		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
-	def speak ()
+	def about
+		puts "\nGender: " + @gender
+		puts "Ethnicity: " + @ethnicity
+		puts "Reindeer Ranking:"
+		@reindeer_ranking.each {|reindeer| puts (@reindeer_ranking.index(reindeer) + 1).to_s + ". " + reindeer}
+		puts "Age: " + @age.to_s
+	end
+
+	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 
@@ -13,6 +24,13 @@ class Santa
 	end
 end
 
-r = Santa.new
-r.speak
-r.eat_milk_and_cookies("Chocolate")
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+santas.each {|santa| santa.about}
