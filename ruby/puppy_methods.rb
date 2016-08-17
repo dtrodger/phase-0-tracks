50d55f86d5d7b6a60aa592f48fa7832c02ed3a98
David Rodgers <dtrodger20@gmail.com>
2016-08-17 16:43:55 -0500

    adds train class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,4 @@
+#PUPPY CLASS
 class Puppy
   def fetch(toy)
     puts "I brought back the #{toy}!"
@@ -16,11 +17,53 @@ class Puppy
   	dog_years = years * 10
   	dog_years
   end
+
+  def jump(feet)
+  	puts "The dog jumped #{feet} feet in the air!"
+  end
 end
 
+#TRAIN CLASS
+class Train
+	def initialize (num_riders, top_speed)
+		puts "Train initialized"
+		@num_riders = num_riders
+		@top_speed = top_speed
+	end
+
+	def add_rider
+		@num_riders += 1
+		puts "There are now #{@num_riders} on the train."
+	end
+
+	def horn
+		puts "CHOO CHOO!"
+	end
+
+	def top_speed
+		puts "This train goes #{@top_speed}"
+	end
+end
+
+#BUSINESS LOGIC
 puppy = Puppy.new
 puppy.fetch("ball")
 puppy.speak(5)
 puppy.roll_over
 puts puppy.dog_years(5)
+puppy.jump
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
