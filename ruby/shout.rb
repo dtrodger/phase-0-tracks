fb628bc5cabd12e5c9b3a1bc309b62cdbac7face
David Rodgers <dtrodger20@gmail.com>
2016-08-22 10:29:49 -0500

    defines shout module as a mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,35 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-  def self.yelling_happily(words)
-    words + "!!!" + " :)"
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#   def self.yelling_happily(words)
+#     words + "!!!" + " :)"
+#   end
 
+# end
+
+# puts Shout.yell_angrily("No")
+# puts Shout.yelling_happily("Yes")
+
+
+module Shout
+	def yell_angrily(words)
+		words + "!!!" + " >:("
+	end
+	def yell_happily(words)
+		words + "!!" + " :)"
+	end
 end
 
-puts Shout.yell_angrily("No")
-puts Shout.yelling_happily("Yes")
+
+class Parent
+	include Shout
+end
+class Kid
+	include Shout
+end
+
+John = Parent.new
+Jill = Kid.new
+puts John.yell_angrily("Go to bed")
+puts Jill.yell_happily("I want dessert")
