4d80da23f212c04e71405149122c4851db2acd81
David Rodgers <dtrodger20@gmail.com>
2016-08-26 09:53:08 -0500

    refactor speed_of_spread to use hash

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -26,40 +26,31 @@ class VirusPredictor
 
   private
 
+  #calculates the number of deaths based on population and mulit
   def num_deaths(multi)
     (@population * multi).floor
   end
 
+  #decides multi based on pop density, calls num_deaths, returns string with numb deaths
   def predicted_deaths
     population_hash = {200 => 0.4, 150 => 0.3, 100 => 0.2, 50 => 0.1, 0 => 0.05}
     population_hash.each do |pop_density, multi|
       if @population_density >= pop_density
         number_of_deaths = num_deaths(multi)
         return "#{@state} will lose #{number_of_deaths} people in this outbreak"
-        break
       end
     end
   end
 
+  #calculates the speed the virus will spread based on population density
   def speed_of_spread #in months
-    # We are still perfecting our formula here. The speed is also affected
-    # by additional factors we haven't added into this functionality.
-    speed = 0.0
-
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
+    speed_hash = {200 => 0.5, 150 => 1, 100 => 1.5, 50 => 2, 0 => 2.5}
+    speed_hash.each do |pop_density, new_speed|
+      if @population_density >= pop_density
+        speed = new_speed
+        return " and will spread across the state in #{speed} months.\n\n"
+      end
     end
-
-    " and will spread across the state in #{speed} months.\n\n"
-
   end
 
 end
@@ -67,20 +58,6 @@ end
 #=======================================================================
 
 # DRIVER CODE
- # initialize VirusPredictor for each state
-
-
-# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-# alabama.virus_effects
-
-# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-# jersey.virus_effects
-
-# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-# california.virus_effects
-
-# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-# alaska.virus_effects
 
 STATE_DATA.each do |state, population_hash|
   new_state = VirusPredictor.new(state, population_hash[:population_density], population_hash[:population])
