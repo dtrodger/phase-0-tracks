cee5cdda1cfea7480c36205f7d0389aaa14340e7
David Rodgers <dtrodger20@gmail.com>
2016-08-29 10:06:16 -0500

    adds funtion that checks if two objects contain matching key value pairs

--- js/algos.js
+++ js/algos.js
@@ -9,5 +9,21 @@ function longestWord(wordArray){
 }
 
 
+function objectsKeyValueEqual(obj1, obj2){
+	for (key in obj1){
+		for (key in obj2){
+			if (obj1[key] == obj2[key]){
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 console.log(longestWord(["a word","something else","the longest phrase"]));
+
+
+console.log(objectsKeyValueEqual({name: "Bob", age: 52},{name: "Joe", age: 54}));
+console.log(objectsKeyValueEqual({name: "Bob", age: 52},{name: "Bob", age: 54}));
+console.log(objectsKeyValueEqual({name: "Bob", age: 52},{name: "Joe", age: 52}));
 
