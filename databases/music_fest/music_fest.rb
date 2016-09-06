f97c85edee27133ac567ac85a94feab12bbd3d8b
David Rodgers <dtrodger20@gmail.com>
2016-09-05 20:28:31 -0500

    owner and venue methods working

--- databases/music_fest/music_fest.rb
+++ databases/music_fest/music_fest.rb
@@ -8,7 +8,7 @@ $db.results_as_hash = true
 
 
 create_owner_table = <<-SQL
-  CREATE TABLE IF NOT EXISTS owners (
+  CREATE TABLE IF NOT EXISTS owner (
     owner_id INTEGER PRIMARY KEY,
     f_name VARCHAR(255),
     l_name VARCHAR(255),
@@ -21,7 +21,7 @@ SQL
 
 
 create_venue_table = <<-SQL
-  CREATE TABLE IF NOT EXISTS venues (
+  CREATE TABLE IF NOT EXISTS venue (
     venue_id INTEGER PRIMARY KEY,
     venue_name VARCHAR(255),
     address VARCHAR(255),
@@ -33,7 +33,7 @@ SQL
 
 
 create_band_table = <<-SQL
-  CREATE TABLE IF NOT EXISTS bands (
+  CREATE TABLE IF NOT EXISTS band (
     band_id INTEGER PRIMARY KEY,
     band_name VARCHAR(255),
     manager_name VARCHAR(255),
@@ -43,7 +43,7 @@ create_band_table = <<-SQL
  SQL
 
 create_show_table = <<-SQL
-  CREATE TABLE IF NOT EXISTS shows (
+  CREATE TABLE IF NOT EXISTS show (
     show_id INTEGER PRIMARY KEY,
     venue_id INTEGER,
     band_id INTEGER,
@@ -58,106 +58,101 @@ $db.execute(create_venue_table)
 $db.execute(create_band_table)
 $db.execute(create_show_table)
 
+def delete_record(table, id)
+  $db.execute("DELETE FROM #{table} WHERE #{table}_id = #{id}")
+end
+
 def create_owner(f_name, l_name, business_name, office_address, phone, email)
-  $db.execute("INSERT INTO owners (f_name, l_name, business_name, office_address, phone, email) VALUES ('#{f_name}', '#{l_name}', '#{business_name}', '#{office_address}', '#{phone}', '#{email}')")
-  puts "New owner created"
+  $db.execute("INSERT INTO owner (f_name, l_name, business_name, office_address, phone, email) VALUES ('#{f_name}', '#{l_name}', '#{business_name}', '#{office_address}', '#{phone}', '#{email}')")
 end
 
 def update_owner(owner_id, f_name, l_name, business_name, office_address, phone, email)
-  $db.execute("UPDATE owners SET f_name='#{f_name}', l_name='#{l_name}', business_name='#{business_name}', office_address='#{office_address}', phone='#{phone}', email='#{email}' WHERE owner_id=#{owner_id}")
-  puts "#{business_name} updated"
+  $db.execute("UPDATE owner SET f_name='#{f_name}', l_name='#{l_name}', business_name='#{business_name}', office_address='#{office_address}', phone='#{phone}', email='#{email}' WHERE owner_id=#{owner_id}")
 end
-
-def delete_owner(owner_id)
-  $db.execute("DELETE FROM owners WHERE owner_id = #{owner_id}")
-  puts "Owner with ID #{owner_id} deleted from owners table"
-end
-
 
 def show_all_owners
-  owners = $db.execute("SELECT * FROM owners")
+  owners = $db.execute("SELECT * FROM owner")
   if owners.any?
     owners.each do |owner|
      puts "\nID: #{owner['owner_id']}\nName: #{owner['f_name']} #{owner['l_name']}\nBusiness Name: #{owner['business_name']}\nOffice Address: #{owner['office_address']}\nPhone: #{owner['phone']}\nEmail: #{owner['email']}\n"
     end
   else
-    puts "The owners table is empty."
+    puts "The owner table is empty."
   end
 end
 
-def create_venue(venue_name, address, venue_capacity, owner_id)
-  $db.execute("INSERT INTO venues (venue_name, address, venue_capacity, owner_id) VALUES ('#{venue_name}', '#{address}', #{venue_capacity}, #{owner_id})")
-  puts "New venue created"
-end
+create_owner("Bill", "Show", "Joes Shows", "123 fake street", "1847-431-0970", "john@srphoto.com")
+# show_all_owners()
+update_owner(1,"vill", "Show", "Joesvdvs", "123 fake street", "1847-431-0970", "john@srphoto.com")
+# show_all_owners()
 
+def create_venue(venue_name, address, venue_capacity, owner_id)
+  $db.execute("INSERT INTO venue (venue_name, address, venue_capacity, owner_id) VALUES ('#{venue_name}', '#{address}', #{venue_capacity}, #{owner_id})")
+end
 
 def update_venue(venue_id, venue_name, address, venue_capacity, owner_id)
-  $db.execute("UPDATE venues SET venue_name='#{venue_name}', address='#{address}', venue_capacity=#{venue_capacity}, owner_id=#{owner_id} WHERE venue_id=#{venue_id}")
-  puts "#{venue_name} updated"
-end
-
-def delete_venue(venue_id)
-  $db.execute("DELETE FROM venues WHERE venue_id = #{venue_id})")
-  puts "Venue with ID #{venue_id} deleted from venue table"
+  $db.execute("UPDATE venue SET venue_name='#{venue_name}', address='#{address}', venue_capacity=#{venue_capacity}, owner_id=#{owner_id} WHERE venue_id=#{venue_id}")
 end
 
 def show_all_venues
-  venues = $db.execute("SELECT * FROM venues")
-  if owners.any?
+  venues = $db.execute("SELECT * FROM venue")
+  if venues.any?
     venues.each do |venue|
      puts "\n\nID: #{venue['venue_id']}\nVenue Name: #{venue['venue_name']}\nVenue Address: #{venue['address']}\nMax Capacity: #{venue['venue_capacity']}\nOwner ID: #{venue['owner_id']}"
     end
   else
-    puts "The venues table is empty."
+    puts "The venue table is empty."
   end
 end
 
-# def create_band (band_name, manager_name, email, phone)
-#   $db.execute("INSERT INTO bands (band_name, manager_name, phone, email) VALUES ('#{@band_name}', '#{@manager_name}', '#{@phone}', '#{@email}')")
-#   puts "New band created"
-# end
+create_venue("this venue", "123 street", 100, 1)
+# show_all_venues()
+update_venue(1, "that venue", "888 street", 200, 1)
+# show_all_venues()
+# delete_record("venue", 1)
+# show_all_venues()
 
-# def update_band(band_id, band_name, manager_name, email, phone)
-#     $db.execute("UPDATE bands SET band_name='#{band_name}', manager_name='#{manager_name}', phone='#{phone}', email='#{email}' WHERE band_id=#{band_id}")
-#     puts "#{band_name} updated"
-# end
+def create_band (band_name, manager_name, email, phone)
+  $db.execute("INSERT INTO band (band_name, manager_name, phone, email) VALUES ('#{band_name}', '#{manager_name}', '#{phone}', '#{email}')")
+end
 
-# def delete_band(band_id)
-#     $db.execute("DELETE FROM bands WHERE band_id = #{band_id})")
-#     puts "Band with ID #{band_id} deleted from bands table"
-# end
+def update_band(band_id, band_name, manager_name, email, phone)
+    $db.execute("UPDATE band SET band_name='#{band_name}', manager_name='#{manager_name}', phone='#{phone}', email='#{email}' WHERE band_id=#{band_id}")
+    puts "#{band_name} updated"
+end
 
-# def show_all_bands
-#   bands = $db.execute("SELECT * FROM bands")
-#   bands.each do |band|
-#   puts "\n\nID: #{band['band_id']}\nBand Name #{band['band_name']}\nManager: #{band['manager_name']}\nContact Email: #{band['email']}\nContact Phone: #{band['phone']}"
-#   end
-#   puts "End of output. If there was no output, the tables is empty."
-# end
+def show_all_bands
+  bands = $db.execute("SELECT * FROM band")
+  if bands.any?
+    bands.each do |band|
+    puts "\n\nID: #{band['band_id']}\nBand Name #{band['band_name']}\nManager: #{band['manager_name']}\nContact Email: #{band['email']}\nContact Phone: #{band['phone']}"
+    end
+  else
+    puts "The band table is empty."
+  end
+end
 
   
-# def create_show (venue_id, band_id, scheduled_time)
-#    $db.execute("INSERT INTO shows (venue_id, band_id, scheduled_time) VALUES (#{@venue_id}, #{@band_id}, '#{@scheduled_time}')")
-#     puts "New owner created"
-#   end
+def create_show (venue_id, band_id, scheduled_time)
+   $db.execute("INSERT INTO show (venue_id, band_id, scheduled_time) VALUES (#{venue_id}, #{band_id}, '#{scheduled_time}')")
+    puts "New owner created"
+  end
 
-# def update_show(show_id, venue_id, band_id, scheduled_time)
-#   $db.execute("UPDATE shows SET venue_id=#{venue_id}, band_id=#{band_id}, scheduled_time='#{scheduled_time}' WHERE show_id=#{show_id}")
-#   puts "Show with id #{show_id} updated"
-# end
+def update_show(show_id, venue_id, band_id, scheduled_time)
+  $db.execute("UPDATE show SET venue_id=#{venue_id}, band_id=#{band_id}, scheduled_time='#{scheduled_time}' WHERE show_id=#{show_id}")
+  puts "Show with id #{show_id} updated"
+end
 
-# def delete_show(show_id)
-#   $db.execute("DELETE FROM shows WHERE show_id = #{show_id})")
-#   puts "Show with ID #{show_id} deleted from shows table"
-# end
-
-# def show_all_shows
-#   shows = $db.execute("SELECT * FROM shows")
-#   shows.each do |show|
-#    puts "\n\nID: #{show['show_id']}\nVenue ID: #{show['venue_id']}\nBand ID: #{show['band_id']}\nScheduled Time: #{show['scheduled_time']}"
-#   end
-#   puts "End of output. If there was no output, the tables is empty."
-# end
+def show_all_shows
+  shows = $db.execute("SELECT * FROM show")
+  if shows.any?
+    shows.each do |show|
+     puts "\n\nID: #{show['show_id']}\nVenue ID: #{show['venue_id']}\nBand ID: #{show['band_id']}\nScheduled Time: #{show['scheduled_time']}"
+    end
+  else
+    puts "End of output. If there was no output, the tables is empty."
+  end
+end
 
 # tom.update(1, "Bill", "kill", "Joes Shows", "123dsadasdtreet", "1847-431-0970", "john@srphoto.com")
 
