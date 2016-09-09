dbf0750a6dc32c279ff1cd9f64bdc9cec6865630
David Rodgers <dtrodger20@gmail.com>
2016-09-09 13:30:33 -0500

    adds campuses table, routes to campuses and new campuses erb, logic to insert campus into campuses table

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -17,11 +17,23 @@ get '/students/new' do
   erb :new_student
 end
 
-# create new students via
-# a form
-post '/students' do
-  db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
-  redirect '/'
+get '/campuses' do
+	@campuses = db.execute("SELECT * FROM campuses")
+	erb :campuses
+end
+
+get '/campuses/new' do
+  erb :new_campus
+end
+
+# post '/students' do
+#   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
+#   redirect '/'
+# end
+
+post '/campuses_post' do
+  db.execute("INSERT INTO campuses (name) VALUES (?)", [params['name']])
+  redirect '/campuses'
 end
 
 # add static resources
