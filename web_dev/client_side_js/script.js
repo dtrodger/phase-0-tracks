05d97f6c57e7a4e6e6ec223174a5f0d865aaceaa
David Rodgers <dtrodger20@gmail.com>
2016-09-08 23:40:41 -0500

    adds js events that hide img and button tags and show imgand button tags

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,9 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
 	var lis = document.getElementsByTagName('li');
-	console.log(lis[0])
-	console.log(lis.length)
 	for (i = 0; i < lis.length; i += 2) {
 		var li = lis[i]
-		li.style.border = "3px solid red"
+		li.style.border = '3px solid red'
 	}
+
+	function hideGhost(event) {
+		event.target.className += 'hidden';
+		var ghost = document.getElementById('ghost');
+		ghost.className += 'hidden';
+		var show = document.getElementById('show');
+		show.className = '';
+	}
+
+	function showGhost(event) {
+		event.target.className += 'hidden';
+		var ghost = document.getElementById('ghost');
+		ghost.className = '';
+		var hide = document.getElementById('hide');
+		hide.className = '';
+	}
+
+	var hide_ghost = document.getElementById('hide');
+	hide_ghost.addEventListener('click', hideGhost);
+
+	var show_ghost = document.getElementById('show');
+	show_ghost.addEventListener('click', showGhost);
+
+
 }, false);
